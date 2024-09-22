7550367de3b7912aa77eb8d4d6902df8c770679b
RicardoDevAlves <motoric750@gmail.com>
2024-09-22 09:00:37 -0300

    confirmando atualização de arquivos

---
+++ 05-projeto-filmes-revisao/src/app/filmes/page.js
@@ -0,0 +1,9 @@
+import { Pagina } from "react-bootstrap";
+
+export default function FilmesPage() {
+
+  return (
+    <Pagina></Pagina>,
+    <p>Este código está sendo chamado pelo componente FILMES...</p>
+  )
+}
---
+++ 05-projeto-filmes-revisao/src/app/page.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import { Button } from "react-bootstrap";
+
+
+
+export default function Home() {
+
+
+
+  return (
+    //código JSX
+    <>
+      <h1>Página inicial de page.js ...</h1>
+      <p>Esta página foi construída dentro do arquivo page.js. Este arquivo é o que está dentro da pasta app de src...</p>
+      <Button>Botão 1</Button>
+      <Button>Botão 2</Button>
+      <p>Dois botões aqui acima, criados com react-bootstrap.</p>
+    </>
+  );
+}
