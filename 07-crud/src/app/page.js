0142bde1c9a7359641679a216ce5ca161430c8e6
RicardoDevAlves <motoric750@gmail.com>
2024-10-03 14:05:09 -0300

    Criando projetos...

---
+++ 07-crud/src/app/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Pagina from "@/components/Pagina";
+
+
+export default function Home() {
+  return (
+    <>
+
+    <Pagina>
+
+    </Pagina>
+    <h1 className="text-center">Página Inicial</h1>
+    <p>Aqui será exibido tudo, pois é a página principal do projeto.</p>
+
+    </>
+  );
+}
